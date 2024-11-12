21974a7eb840a655a3b2bdd91536ca1ca44878e0
Davod0 <davodnikoyii@gmai.com>
2024-11-12 13:58:09 +0100

    Done with the implementation of middleware

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+import { ValidStudentMiddleware } from './../ommon/middleware/validStudent.middleware';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
 
 @Module({
-    controllers: [StudentController],
-    providers:[StudentService],
-    exports: [StudentService]
+  controllers: [StudentController],
+  providers: [StudentService],
+  exports: [StudentService],
 })
-export class StudentModule {}
+export class StudentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    console.log('This middleware is called')
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: 'students/:studentId',
+      method: RequestMethod.GET,
+    });
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: 'students/:studentId',
+      method: RequestMethod.PUT,
+    });
+  }
+}
